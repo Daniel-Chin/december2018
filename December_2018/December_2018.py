53b49fca8645bcf2659f2262dcfcc464def1948f
秦Daniel <daniel_chin@yahoo.com>
2019-01-09 19:24:03 -0500

    complete game_state

--- December_2018/December_2018.py
+++ December_2018/December_2018.py
@@ -4,4 +4,19 @@ import os
 from os import path
 os.chdir(path.dirname(__file__))
 os.chdir('data')
-import file
+import game_state
+import sys
+from time import sleep
+
+def main():
+    ...
+
+if __name__ == '__main__':
+    main()
+    sys.exit(0)
+else:
+    print('What??? '); sleep(1)
+    print('Why did you import my game? '); sleep(1)
+    print('Dirty hacker! '); sleep(1)
+    class DirtyHackerError(Exception):pass
+    raise DirtyHackerError
---
+++ December_2018/data/game_state.py
@@ -0,0 +1,65 @@
+import pickle
+import sys
+from io import BytesIO
+
+INIT_STATE = {}
+
+state = {}
+
+def load():
+    global state
+    try:
+        with open('file0', 'rb') as f0:
+            with open('file1', 'rb') as f1:
+                with open('file9', 'rb') as f9:
+                    which = None
+                    try:
+                        which = f9.read()[0]
+                        if which not in (0, 1):
+                            which = None
+                            raise IndexError
+                        state = pickle.load((f0, f1)[which])
+                        copy(f0, f1, which)
+                    except IndexError:
+                        try:
+                            state = pickle.load(f0)
+                            which = 0
+                        except:
+                            try:
+                                state = pickle.load(f1)
+                                which = 1
+                            except:
+                                print('Game save file corrupted - which is unbelievable, because Daniel wrote an entire python script dedicated to prevent this from happening. Contact Daniel, and tell him what heppened. Please. ')
+                                input('Press Enter to exit game... ')
+                                sys.exit(1207)
+        assert which is not None
+        f0.seek(0)
+        f1.seek(0)
+        if f0.read() != f1.read():
+            goodFile = (f0, f1)[which]
+            badFile = (f0, f1)[1 - which]
+            goodFile.seek(0)
+            data = goodFile.read()
+            badFile.close()
+            badFile = open('file' + str(1 - which), 'wb')
+            badFile.write(data)
+            badFile.close()
+    except FileNotFoundError:
+        state = INIT_STATE
+        save()
+
+def save():
+    io = BytesIO()
+    pickle.dump(state, io)
+    data = io.read()
+    io.close()
+    with open('file9', 'wb+') as f:
+        f.write(b'\x01')
+    with open('file0', 'wb+') as f:
+        f.write(data)
+    with open('file9', 'wb+') as f:
+        f.write(b'\x00')
+    with open('file1', 'wb+') as f:
+        f.write(data)
+
+load()
