72ae6b4db8a0143bd3b8ada84bade38d0a6c398d
秦Daniel <daniel_chin@yahoo.com>
2019-01-09 23:05:36 -0500

    install.getPip done

--- December_2018/data/install.py
+++ December_2018/data/install.py
@@ -3,8 +3,16 @@ from time import sleep
 from subprocess import Popen
 import sys
 import platform
+from os import system as terminal
+
+if platform.system() == 'Windows':
+    prompt = '> '
+else:
+    prompt = '$ '
 
 def main():
+    if gameState['install']['stage'] > 2:
+        typeWrite('* Welcome back. \n  We shall continue. '.upper())
     while gameState['install']['stage'] != len(STAGES):
         STAGES[gameState['install']['stage']]()
         gameState['install']['stage'] += 1
@@ -13,46 +21,101 @@ def main():
     saveState()
 
 def greet():
-    typeWrite('Thank you. ', interval = 0.1)
+    typeWrite('* Thank you. '.upper())
     sleep(1.5)
-    typeWrite('Thank you for starting', end = ' ', interval = 0.1)
+    typeWrite('* Thank you for starting'.upper(), end = ' ')
     sleep(1)
-    typeWrite('to play this game. ', interval = 0.1)
+    typeWrite('to play this game. '.upper())
     sleep(1)
     print()
-    typeWrite('For this game to run, let us install some dependencies. ')
+    typeWrite('* This game needs some dependencies. '.upper())
     sleep(1)
-    typeWrite('First of all, we need pip. ')
+    typeWrite('* Let us install them one by one. '.upper())
+    sleep(1)
+
+def getPip():
+    typeWrite('* First of all, we need pip. '.upper())
+    sleep(1)
     try:
-        Popen('python3 -V', stdout = 0)
-        desired = 'python3'
+        python_name = 'python3'
+        Popen([python_name, '-V'], stdout = open('temp', 'w+'))
     except FileNotFoundError:
         try:
-            Popen('python -V', stdout = 0)
-            desired = 'python'
+            python_name = 'python'
+            Popen([python_name, '-V'], stdout = open('temp', 'w+'))
         except FileNotFoundError:
             print('Fatal Error: cannot call python from terminal. Add python to path, please! ')
             input('Press Enter to exit game. ')
             sys.exit(7482)
-    desired += ' get-pip.py'
-    if platform.system() == 'Windows':
-        prompt = '> '
-    else:
-        prompt = '$ '
+    desired = python_name + ' get-pip.py --user'
     op = ''
     while op != desired:
-        typeWrite('Please type "%s" and press Enter: ' % desired)
+        typeWrite('* PLEASE TYPE "%s" AND PRESS ENTER. ' % desired)
         op = input(prompt)
-    Popen(op)
-    import pip
-    input('ok')
+    popen = Popen(op.split(' '))
+    typeWrite('* Now wait...'.upper())
+    popen.wait()
+    gameState['install']['stage'] += 1
+    saveState()
+    terminal(python_name + ' ../December_2018.py')
+    sys.exit(0)
 
-def typeWrite(*args, interval = 0.06, end = '\n', sep = ' '):
+def verifyPip():
+    global pip
+    try:
+        import pip
+        if not hasattr(pip, 'main'):
+            import pip._internal as pip
+    except:
+        print('Fatal Error: Failed to install pip! ')
+        input('Press Enter to exit game. ')
+        import pip
+        sys.exit(4685)
+    print()
+    typeWrite('* Good job. '.upper())
+
+def pipInstall(*packages):
+    pip.main(['install'] + packages + ['--user'])
+
+def typeWrite(*args, interval = 0.1, end = '\n', sep = ' '):
     for char in sep.join(args):
         print(char, end = '', flush = True)
         sleep(interval)
     print('', end = end, flush = True)
 
+def commandPreview(*packages):
+    return '%spip install %s --user' % (prompt, ' '.join(packages))
+
+def getColor():
+    global colorama
+    typeWrite('* Next, colors. '.upper())
+    sleep(1)
+    typeWrite('* I WILL RUN "%s". ' % commandPreview('colorama'))
+    sleep(1)
+    typeWrite('* Do you give consent? '.upper())
+    if getConsent():
+        print(commandPreview('colorama'))
+        pipInstall('colorama')
+        import colorama
+        from colorama import Fore, Back, Style
+        colorama.init()
+        print(Fore.RED, Back.WHITE, Style.DIM)
+        typeWrite('* Great. '.upper(), end = '')
+        print(Style.RESET_ALL)
+    else:
+        typeWrite('* I am sure '.upper(), end = '')
+        sleep(0.6)
+        typeWrite(' you will come back again. '.upper())
+        sys.exit(0)
+
+def getConsent():
+    op = input('y/n: ').lower()
+    while op not in ('y', 'n'):
+        op = input('Please type either "y" or "n" and press Enter: ').lower()
+    return op == 'y'
+
 STAGES = [
     greet, 
+    getPip, 
+    verifyPip, 
 ]
