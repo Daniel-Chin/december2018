2ce81a7891b7db20d711bb55607275095a3ac21f
秦Daniel <daniel_chin@yahoo.com>
2019-01-09 21:24:08 -0500

    keep working on install.greet

--- December_2018/data/install.py
+++ December_2018/data/install.py
@@ -1,5 +1,8 @@
 from .game_state import *
 from time import sleep
+from subprocess import Popen
+import sys
+import platform
 
 def main():
     while gameState['install']['stage'] != len(STAGES):
@@ -10,14 +13,42 @@ def main():
     saveState()
 
 def greet():
-    typeWrite('Thank you. ')
-    sleep(2)
-    typeWrite('Thank you for starting', end = ' ')
+    typeWrite('Thank you. ', interval = 0.1)
+    sleep(1.5)
+    typeWrite('Thank you for starting', end = ' ', interval = 0.1)
     sleep(1)
-    typeWrite('to play this game. ')
+    typeWrite('to play this game. ', interval = 0.1)
+    sleep(1)
+    print()
+    typeWrite('For this game to run, let us install some dependencies. ')
+    sleep(1)
+    typeWrite('First of all, we need pip. ')
+    try:
+        Popen('python3 -V', stdout = 0)
+        desired = 'python3'
+    except FileNotFoundError:
+        try:
+            Popen('python -V', stdout = 0)
+            desired = 'python'
+        except FileNotFoundError:
+            print('Fatal Error: cannot call python from terminal. Add python to path, please! ')
+            input('Press Enter to exit game. ')
+            sys.exit(7482)
+    desired += ' get-pip.py'
+    if platform.system() == 'Windows':
+        prompt = '> '
+    else:
+        prompt = '$ '
+    op = ''
+    while op != desired:
+        typeWrite('Please type "%s" and press Enter: ' % desired)
+        op = input(prompt)
+    Popen(op)
+    import pip
+    input('ok')
 
-def typeWrite(text, interval = 0.1, end = '\n'):
-    for char in text:
+def typeWrite(*args, interval = 0.06, end = '\n', sep = ' '):
+    for char in sep.join(args):
         print(char, end = '', flush = True)
         sleep(interval)
     print('', end = end, flush = True)
